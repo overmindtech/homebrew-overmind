26fb7b288a2ab2e6dd935f186f2e0b8191e89488
Dylan Ratcliffe <dylan@overmind.tech>
2024-03-01 17:23:37 +0000

    added formula

---
+++ Formula/overmind-cli.rb
@@ -0,0 +1,24 @@
+class OvermindCli < Formula
+  desc "CLI to interact with the Overmind API"
+  homepage "https://overmind.tech/"
+  url "https://github.com/overmindtech/cli/archive/refs/tags/v0.25.11.tar.gz"
+  sha256 "7fa29704956ec67b776c9394f26c27a3dfa324745a38901b3dde8c3ad3d74427"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    # Write the version of the package to a file so that it can be compiled into
+    # the binary
+    File.write("tracing/commit.txt", "v#{version}-brew")
+    File.write("cmd/commit.txt", "v#{version}-brew")
+
+    system "go", "build", *std_go_args(ldflags: "-s -w", output: "overmind")
+
+    bin.install "overmind"
+  end
+
+  test do
+    system "overmind", "--version"
+  end
+end
