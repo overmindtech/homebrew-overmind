23e278645b32cfbe84feb8f048217b34a49aa762
Dylan <8799341+dylanratcliffe@users.noreply.github.com>
2024-03-04 11:49:16 +0000

    overmind-cli: add 0.25.17 bottle.

--- Formula/overmind-cli.rb
+++ Formula/overmind-cli.rb
@@ -5,6 +5,13 @@ class OvermindCli < Formula
   sha256 "a366ceca8e810325bff8de213cb7f448b24b56954482bda1bd4c5ab36811c625"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/overmindtech/overmind"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "5d3bf8e3916232633229fe5cbc906476fc11e970ede0bcabc89e39eea595884a"
+    sha256 cellar: :any_skip_relocation, ventura:      "25338191f31e4e828e39e105291f2aeeaf240542e89f9ea5e200f00fb2f43c5e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6ec3168f5b67e27e31eaee5a2ccf86811a4384a57a731af8eace34007e1cd0d6"
+  end
+
   depends_on "go" => :build
 
   def install
