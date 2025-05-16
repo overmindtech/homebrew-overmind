07923334caeab02e49e20204f617c3fafbefe1c1
TP Honey <thomas.honey@overmind.tech>
2025-05-16 09:36:36 +0000

    overmind-cli: update 1.8.1 bottle.

--- Formula/overmind-cli.rb
+++ Formula/overmind-cli.rb
@@ -7,10 +7,10 @@ class OvermindCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/overmindtech/overmind"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "43aa850ebddc61989c3c3ad5a0de7663c80939bc747ff349842e07443d550185"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0d9dfc7b9447129b6040be9656d4c50f1dbe4d17c87448e0565c90b5b61fc9e0"
-    sha256 cellar: :any_skip_relocation, ventura:       "7c153fb993d37b2911d4d7c22b5e2b8d573bb02b31353dd527b57b9f8732b094"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e336133102abc7475f3a071b02b1bb2acffd0785476ba066fc6d86c41ad7200d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "11d80d69b1a403dcebf78ee1519f2a23a5360acc62228e57c3aef5b0524e9b6b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "620c471eb9bd5e967f79e24c1b03ba831f27dfd36164707934302498e7ca10e1"
+    sha256 cellar: :any_skip_relocation, ventura:       "abdef5a57b46c76c095ef1a174b378cbea66c00a668c68db04c7dd57e9b365b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ba2f1d07d8b21468983f0a76fad7458ac29215a317c809bfc1b792c7bd1d082f"
   end
 
   depends_on "go" => :build
