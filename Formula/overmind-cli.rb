14f68c8a8e8e700c0d2d5b2d39b719f1679807f2
Dylan <8799341+dylanratcliffe@users.noreply.github.com>
2026-01-20 23:18:59 +0000

    overmind-cli: update 1.13.1 bottle.

--- Formula/overmind-cli.rb
+++ Formula/overmind-cli.rb
@@ -7,9 +7,9 @@ class OvermindCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/overmindtech/overmind"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4221a39b2259d1345c826e7cd6a2bac6bb376e8ee719e4696121b5d82d0c995b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5cb27e1c0489c06ad6878990bbadd959993104523d8955688e7ba0dd4035e202"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d1dd23f0813b508ed20a071d9dd696c2807fcc36d5cfea8e2da0e020010c554"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0dc05a5dd1eadffd7e1be5aa93ed4ecc9efc4f7f2214233434307181b44e79b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "514ac56244495df4f3e99ad763d3c8e4aeb0c851e6b9d4e125823d30be55a785"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02f32a960944c08c1440bf26ac5d9145fac5cc9b1dac7985671772342e6f957a"
   end
 
   depends_on "go" => :build
